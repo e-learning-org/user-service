308c85fbafa28cd1beeffee141b50727a555444d
Omar Fostok <omar.fostok@ovrlab.io>
2025-07-03 20:24:31 +0300

    project initilization

---
+++ src/main/kotlin/com/elearning/user_service/UserController.kt
@@ -0,0 +1,13 @@
+package com.elearning.user_service
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/users")
+class UserController {
+
+    @GetMapping
+    fun test(): String = "Hello World!"
+}
